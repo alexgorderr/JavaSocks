5d6d664c784decdccce43d2f0f0ea18f02908de6
alexgorderr <42914584+alexgorderr@users.noreply.github.com>
2020-07-09 18:32:09 +0300

    Update GUI.java

--- src/main/java/GUI/GUI.java
+++ src/main/java/GUI/GUI.java
@@ -5,16 +5,19 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.Random;
 import java.util.Random.*;
+import java.util.Scanner;
+
 import Graph.*;
 
 public class GUI {
     private Graph graph;
     private JLabel labelIn = new JLabel("Input matrix:");
-    private JTextArea textIn = new JTextArea("2 3 1 5\n0 1 7 -3\n2 1 -4 3\n2 5 3 0");
+    private JTextArea textIn = new JTextArea("2 3 1 5\n0 1 7 4\n2 1 1 3\n2 5 3 0");
     private JLabel labelOut = new JLabel("Output matrix:");
     private JTextArea textOut = new JTextArea();
 
-    private JSpinner spinner = new JSpinner();
+    private JSpinner spinnerV = new JSpinner();
+    private JSpinner spinnerE = new JSpinner();
 
     private JButton InputButton = new JButton("Input");
     private JButton RandomButton = new JButton("Random");
@@ -33,14 +36,15 @@ public class GUI {
     private JButton changeButton = new JButton("Change");
     private JTextField delVertexText = new JTextField("", 7);
     private JButton delVertexButton = new JButton("Delete vertex");
-
+    private JTextField delEdgeText = new JTextField("", 5);
+    private JButton delEdgeButton = new JButton("Delete edge");
 
     private Vizualizator visual = new Vizualizator();
 
     private Container container = new Container();
     public GUI() {
         JFrame frame = new JFrame("Program");
-        frame.setBounds(100, 100, 1640, 1480);
+        frame.setBounds(0, 0, 1240, 640);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         container = frame.getContentPane();
@@ -55,7 +59,13 @@ public class GUI {
 
         Container westContainer = new Container(); // ввод
         westContainer.setLayout(new GridLayout(3, 2));
-        westContainer.add(spinner);
+        Container rand = new Container();
+        rand.setLayout(new FlowLayout());
+        rand.add(new JLabel("Vertexes"));
+        rand.add(spinnerV);
+        rand.add(new JLabel("Edges"));
+        rand.add(spinnerE);
+        westContainer.add(rand);
         RandomButton.addActionListener(new RandomActionListener());
         westContainer.add(RandomButton);
         westContainer.add(labelIn);
@@ -69,15 +79,22 @@ public class GUI {
 
 
         Container eastContainer = new Container();
-        eastContainer.setLayout(new GridLayout(4, 2));
+        eastContainer.setLayout(new GridLayout(5, 2));
         eastContainer.add(new JPanel());
+        addVertex.addActionListener(new addVertexListener());
         eastContainer.add(addVertex);
         eastContainer.add(addEdgeText);
+        addEdgeButton.addActionListener(new addEdgeListener());
         eastContainer.add(addEdgeButton);
         eastContainer.add(changeEdgeText);
+        changeButton.addActionListener(new changeButtonListener());
         eastContainer.add(changeButton);
         eastContainer.add(delVertexText);
+        delVertexButton.addActionListener(new delVertexListener());
         eastContainer.add(delVertexButton);
+        eastContainer.add(delEdgeText);
+        delEdgeButton.addActionListener(new delEdgeListener());
+        eastContainer.add(delEdgeButton);
 
 
         Container southContainer = new Container();
@@ -98,7 +115,7 @@ public class GUI {
         frame.setVisible(true);
     }
     // тут классы "реагенты" кнопочек
-    class InputButtonListener implements ActionListener {
+    class InputButtonListener implements ActionListener { // тоже можно проверить
         public void actionPerformed (ActionEvent e) {
 
             if(textIn.getText().length() == 0) {
@@ -116,19 +133,24 @@ public class GUI {
         }
     }
 
-    class RandomActionListener implements ActionListener {
+    class RandomActionListener implements ActionListener { // тоже можно проверить
         public void actionPerformed (ActionEvent e) {
-            int num = (int)spinner.getValue();
-            if(num <= 0) {
-                JOptionPane.showMessageDialog(null, "Null matrix", "Message", JOptionPane.PLAIN_MESSAGE);
+            int vertexes = (int)spinnerV.getValue();
+            int edges = (int)spinnerE.getValue();
+            if(vertexes <= 0) {
+                JOptionPane.showMessageDialog(null, "Wrong vertexes input", "Message", JOptionPane.PLAIN_MESSAGE);
+            }
+            else if(edges <= 0 || edges >= vertexes*(vertexes-1)) {
+                JOptionPane.showMessageDialog(null, "Wrong edges input", "Message", JOptionPane.PLAIN_MESSAGE);
             }
             else {
-                int[][] matr = new int[num][num];
+                int[][] matr = new int[vertexes][vertexes];
                 String str = "";
-                for(int i = 0; i < num; i++) {
-                    for(int j = 0; j < num; j++) {
+                for(int i = 0; i < vertexes; i++) {
+                    for(int j = 0; j < vertexes; j++) {
                         Random rand = new Random();
-                        matr[i][j] = rand.nextInt(20);
+                        matr[i][j] = (edges > 0 ? rand.nextInt(20) : 0);
+                        edges--;
                         str+=matr[i][j] + " ";
                     }
                     str+= "\n";
@@ -144,16 +166,50 @@ public class GUI {
         }
     }
 
-    class StepListener implements ActionListener {
+    class StepListener implements ActionListener { // проверить и имплеменитровать графику
         public void actionPerformed(ActionEvent e) {
             graph.FWStep();
-            ;;
         }
     }
 
-    class QuickListener implements ActionListener {
+    class QuickListener implements ActionListener { // проверить и имплементировать графику
         public void actionPerformed(ActionEvent e) {
             graph.FloydWarshall();
+            visual.initMatrix(graph.getMatrix(), graph.getMatrix().length);
+        }
+    }
+
+    class addVertexListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            int[][] matr = graph.getMatrix();
+            int num = matr.length;
+            matr = new int[num+1][num+1];
+            // не закончено
+        }
+    }
+    class addEdgeListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            int from, to, weight;
+            Scanner in = new Scanner(addEdgeText.getText());
+            from = in.nextInt();
+            to = in.nextInt();
+            weight = in.nextInt();
+            // не закончено
+        }
+    }
+    class changeButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            // не сделано
+        }
+    }
+    class delVertexListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            // не сделано
+        }
+    }
+    class delEdgeListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            // не сделано
         }
     }
 
