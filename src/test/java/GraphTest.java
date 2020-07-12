72ab99e4c2c4b22ebf1791045289f53602b8c964
mashaaaa1 <poturaevamasha@icloud.com>
2020-07-12 18:48:02 +0300

    test

--- src/test/java/GraphTest.java
+++ src/test/java/GraphTest.java
@@ -19,9 +19,9 @@ class GraphTest {
     @Test
     void floydWarshall() {
         graph.FloydWarshall();
-        assertEquals(" #  1  2  3  4  \n1  0  12  3  4  \n2  1  0  4  5  \n3  5  9  0  1  \n4  4  8  7  0  \n",graph.print());
+        assertEquals(" #  1  2  3  4  \n1  0  12 3  4  \n2  1  0  4  5  \n3  5  9  0  1  \n4  4  8  7  0  \n",graph.print());
         graph1.FloydWarshall();
-        assertEquals(" #  1  2  3  4  5  6  \n1  0  10  18  8  31  20  \n2  10  0  16  9  21  21  \n3  26  16  0  25  37  15  \n4  7  9  25  0  30  12  \n5  64  54  38  63  0  23  \n6  41  31  15  40  23  0  \n",graph1.print());
+        assertEquals(" #  1  2  3  4  5  6  \n1  0  10 18 8  31 20 \n2  10 0  16 9  21 21 \n3  26 16 0  25 37 15 \n4  7  9  25 0  30 12 \n5  64 54 38 63 0  23 \n6  41 31 15 40 23 0  \n",graph1.print());
     }
 
 }
