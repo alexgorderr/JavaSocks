03bf9393535b70aea95d951f450252e70939623c
alexgorderr <42914584+alexgorderr@users.noreply.github.com>
2020-07-12 13:35:46 +0300

    Set prettier appearence of gui

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,18 @@
 import GUI.*;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (Exception exception) {
+            System.err.println("Cannot make it prettier");
+        }
         GUI gui = new GUI();
+
     }
 }
